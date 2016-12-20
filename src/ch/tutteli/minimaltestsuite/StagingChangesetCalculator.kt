23e13c9a0c41aee8dbd76ae8df91e81051b5d5a0
Robert Stoll <rstoll@tutteli.ch>
2016-12-20 18:47:56 +0100

    added jacoco and travis and fixed a visibility issue

--- src/ch/tutteli/minimaltestsuite/StagingChangesetCalculator.kt
+++ src/ch/tutteli/minimaltestsuite/StagingChangesetCalculator.kt
@@ -2,7 +2,7 @@ package ch.tutteli.minimaltestsuite
 
 import org.eclipse.jgit.api.Git
 
-class StagingChangesetCalculator(val git: Git) : ChangesetCalculator {
+class StagingChangesetCalculator(private val git: Git) : ChangesetCalculator {
     override fun calculate(): Set<String> {
         val status = git.status().call()
         return status.added + status.changed
